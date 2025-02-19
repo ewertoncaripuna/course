bfcb83f53384fb82628392ed5ca0a6f5c45c2f07
EwertonCaripuna <ewertoncaripuna@outlook.com>
2025-02-19 15:35:36 -0300

    feat: Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +46,15 @@ public class UserService {
         }
 
     public User update(Long id, User obj){
+        try{
         User entity = userRepository.getReferenceById(id);
         updateData(entity, obj);
         return userRepository.save(entity);
+
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+
+        }
     }
 
     private void updateData(User entity, User obj) {
